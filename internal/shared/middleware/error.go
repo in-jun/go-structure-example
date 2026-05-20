1c21f241a2d658e40d7cbbb1baf9e265a87dd6af
in-jun <me@injun.dev>
2026-05-20 21:56:01 +0900

    feat: log and scrub internal errors in error middleware
    500 errors are now logged with slog and their message is replaced with
    a generic "Internal Server Error" before sending to the client, preventing
    internal detail leakage. Unhandled (non-CustomError) errors are also
    logged. Matches auction example pattern.

--- internal/shared/middleware/error.go
+++ internal/shared/middleware/error.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	stderrors "errors"
+	"log/slog"
 
 	"github.com/gin-gonic/gin"
 	"github.com/in-jun/go-structure-example/internal/shared/errors"
@@ -11,14 +12,22 @@ func ErrorHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next()
 
-		if len(c.Errors) > 0 {
-			err := c.Errors.Last().Err
-			var customErr errors.CustomError
-			if stderrors.As(err, &customErr) {
-				c.JSON(customErr.Status, customErr)
-				return
-			}
-			c.JSON(500, errors.Internal("Internal Server Error"))
+		if len(c.Errors) == 0 {
+			return
 		}
+
+		err := c.Errors.Last().Err
+		var customErr errors.CustomError
+		if stderrors.As(err, &customErr) {
+			if customErr.Status >= 500 {
+				slog.Error("internal error", "message", customErr.Message)
+				customErr.Message = "Internal Server Error"
+			}
+			c.JSON(customErr.Status, customErr)
+			return
+		}
+
+		slog.Error("unhandled error", "error", err)
+		c.JSON(500, errors.Internal("Internal Server Error"))
 	}
 }
